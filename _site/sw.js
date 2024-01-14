7ea25dc66a3afecb73448dd8b56a3a4f219c5cf4
xudong-zhu01 <155582855+xudong-zhu01@users.noreply.github.com>
2024-01-14 17:31:17 +0100

    1

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705249443';
+const cacheName = 'chirpy-1705249870';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
